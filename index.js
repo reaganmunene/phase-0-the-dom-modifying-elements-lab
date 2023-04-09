7830ff54152a02dadef71a75ddf923f6d9f6fd71
reaganmunene <reagannmunene@gmail.com>
2023-04-09 11:03:51 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code here!
+const mainIdRemove = document.getElementById("main");
+mainIdRemove.remove();
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.textContent = "Reagan is the champion";
